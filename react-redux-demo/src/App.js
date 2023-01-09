fddd13b5c9bfc14c1fcbc1ac3b49dc960b6ab761
Zankhi <zankj2019@gmail.com>
2023-01-09 19:40:36 +0530

    use of slice and reduxjs toolkit

--- react-redux-demo/src/App.js
+++ react-redux-demo/src/App.js
@@ -1,15 +1,16 @@
 import './App.css';
 import { useSelector, useDispatch } from 'react-redux';
+import { actions } from './store';
 
 function App() {
   const counter = useSelector((state) => state.counter);
   const dispatch = useDispatch();
   return (
     <div>
-      <button onClick={() => dispatch({type: "INCREMENT"})}>INCREMENT</button>
+      <button onClick={() => dispatch(actions.increment())}>INCREMENT</button>
       <h1>Counter : {counter} </h1>
-      <button onClick={() => dispatch({type: "DECREMENT"})}>DECREMENT</button>
-      <button onClick={() => dispatch({type: "ADDBY10", payload: 10})}>ADDBY10</button>
+      <button onClick={() => dispatch(actions.decrement())}>DECREMENT</button>
+      <button onClick={() => dispatch(actions.addBy10(10))}>ADDBY10</button>
     </div>
   );
 }
--- react-redux-demo/src/store/index.js
+++ react-redux-demo/src/store/index.js
@@ -1,17 +1,40 @@
-import { createStore } from "redux";
+import { configureStore, createSlice } from "@reduxjs/toolkit";
 
-const reducerFn = (state = {counter: 0}, action) => {
-    if (action.type === 'INCREMENT') {
-        return {counter: state.counter + 1};
+const counterSlice = createSlice({
+    name: "counter",
+    initialState: { counter: 0 },
+    reducers: {
+        increment(state) {
+            return { counter: state.counter + 1 };
+        },
+        decrement(state) {
+            return { counter: state.counter - 1 };
+        },
+        addBy10(state, action) {
+            return { counter: state.counter + action.payload };
+        }
     }
-    if (action.type === 'DECREMENT') {
-        return {counter : state.counter - 1}
-    } 
-    if (action.type === 'ADDBY10') {
-        return {counter : state.counter + action.payload}
-    }
-    return state;
-}
-const store = createStore(reducerFn);
+});
 
+const store = configureStore({
+    reducer: counterSlice.reducer
+});
+
+export const actions = counterSlice.actions;
 export default store;
+
+// const reducerFn = (state = {counter: 0}, action) => {
+//     if (action.type === 'INCREMENT') {
+//         return {counter: state.counter + 1};
+//     }
+//     if (action.type === 'DECREMENT') {
+//         return {counter : state.counter - 1}
+//     } 
+//     if (action.type === 'ADDBY10') {
+//         return {counter : state.counter + action.payload}
+//     }
+//     return state;
+// }
+// const store = createStore(reducerFn);
+
+// export default store;
